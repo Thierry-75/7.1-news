f79cc80c0fcc729d3a37512a713ebbf9f534ad84
thierry dothee <thierrydothee@protonmail.com>
2024-10-26 06:30:12 +0200

    fixtures ok

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -8,27 +8,33 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
+use Faker\Generator;
 
 class PostFixtures extends Fixture implements DependentFixtureInterface
 {
     private SluggerInterface $slugger;
+    private Generator $faker;
 
     public function __construct(SluggerInterface $slugger)
     {
         $this->slugger = $slugger;
+        $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
+        for($i=0; $i < 24; $i++){
         $post = new Article();
-        $post->setTitre('Ceci est un titre')
+        $post->setTitre($this->faker->sentence())
             ->setSlug(strtolower($this->slugger->slug($post->getTitre())))
-            ->setContenu('Ceci est le contenu')
+            ->setContenu($this->faker->text(1000))
             ->setUser($this->getReference('admin'))
             ->setFeaturedImage('default.webp')
             ->setCreateAt(new \DateTimeImmutable());
 
         $manager->persist($post);
+        }
         $manager->flush();
     }
 
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,22 +2,38 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Generator;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class UserFixtures extends Fixture
 {
-    private UserPasswordHasherInterface $passwordHasher;
+    private Generator $faker;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    public function __construct()
     {
-        $this->passwordHasher = $passwordHasher;
+        $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
+
+        for ($i = 0; $i < 3; $i++) {
+            $user  = new User();
+            $user->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setCreateAt(new \DateTimeImmutable())
+                ->setPlainpassword('ArethiA75!')
+                ->setZip(str_replace(' ', '', $this->faker->postcode()))
+                ->setCity($this->faker->city())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstNameFemale() . '-' . mt_rand(1, 99) : $this->faker->firstNameMale() . '-' . mt_rand(0, 99))
+                ->setIsVerified(mt_rand(0, 1) === 1 ? true : false);
+            $users[] = $user;
+            $manager->persist($user);
+        }
+
         $user = new User();
         $user->setPseudo('user')
         ->setEmail('user@demo.fr')
